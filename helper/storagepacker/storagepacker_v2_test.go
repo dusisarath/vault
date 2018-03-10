d29a980279c68981f29bd97cb8eb5697812ca5b9
vishalnayak <vishalnayakv@gmail.com>
2018-03-10 07:20:36 -0500

    refactor tests

--- helper/storagepacker/storagepacker_v2_test.go
+++ helper/storagepacker/storagepacker_v2_test.go
@@ -3,6 +3,7 @@ package storagepacker
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strconv"
 	"testing"
 
@@ -13,24 +14,13 @@ import (
 	log "github.com/mgutz/logxi/v1"
 )
 
-func TestStoragePackerV2_PutGet(t *testing.T) {
-	sp, err := NewStoragePackerV2(&Config{
-		View:   &logical.InmemStorage{},
-		Logger: log.New("storagepackertest"),
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Write 100 items; check if it can be read back out
-	for i := 1; i <= 100; i++ {
+func testPutItem(t *testing.T, sp *StoragePackerV2, count int, entity *identity.Entity) {
+	t.Helper()
+	for i := 1; i <= count; i++ {
 		id := strconv.Itoa(i)
+		entity.ID = id
 
-		message := &identity.Entity{
-			ID: id,
-		}
-
-		marshaledMessage, err := ptypes.MarshalAny(message)
+		marshaledMessage, err := ptypes.MarshalAny(entity)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -43,17 +33,31 @@ func TestStoragePackerV2_PutGet(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		bucketKey, err := sp.PutItem(item)
+		_, err = sp.PutItem(item)
 		if err != nil {
 			t.Fatal(err)
 		}
+	}
+}
 
+func testGetItem(t *testing.T, sp *StoragePackerV2, count int, expectNil bool) {
+	t.Helper()
+	for i := 1; i <= count; i++ {
+		id := strconv.Itoa(i)
 		itemFetched, err := sp.GetItem(id)
 		if err != nil {
 			t.Fatal(err)
 		}
+
+		switch expectNil {
+		case itemFetched == nil:
+			return
+		default:
+			t.Fatalf("expected nil for item %q\n", id)
+		}
+
 		if itemFetched == nil {
-			t.Fatalf("failed to read the inserted item; iteration: %d, bucketKey: %q, itemID: %q\n", i, bucketKey, id)
+			t.Fatalf("failed to read the inserted item %q", id)
 		}
 
 		var fetchedMessage identity.Entity
@@ -66,6 +70,105 @@ func TestStoragePackerV2_PutGet(t *testing.T) {
 			t.Fatalf("failed to fetch item ID: %q\n", id)
 		}
 	}
+}
+
+func testDeleteItem(t *testing.T, sp *StoragePackerV2, count int) {
+	t.Helper()
+	for i := 1; i <= count; i++ {
+		if i%500 == 0 {
+			fmt.Printf("delete item iteration: %d\n", i)
+		}
+		id := strconv.Itoa(i)
+		err := sp.DeleteItem(id)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func TestStoragePackerV2_Put(t *testing.T) {
+	sp, err := NewStoragePackerV2(&Config{
+		View:   &logical.InmemStorage{},
+		Logger: log.New("storagepackertest"),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	entity := &identity.Entity{
+		Metadata: map[string]string{
+			"samplekey": "samplevalue",
+		},
+	}
+	testPutItem(t, sp, 100, entity)
+}
+
+func TestStoragePackerV2_PutGetDeleteInmem(t *testing.T) {
+	sp, err := NewStoragePackerV2(&Config{
+		View:   &logical.InmemStorage{},
+		Logger: log.New("storagepackertest"),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	entity := &identity.Entity{
+		Metadata: map[string]string{
+			"samplekey": "samplevalue",
+		},
+	}
+	count := 100
+	testPutItem(t, sp, count, entity)
+	testGetItem(t, sp, count, false)
+	testDeleteItem(t, sp, count)
+	testGetItem(t, sp, count, true)
+}
+
+func TestStoragePackerV2_PutGetDelete_File(t *testing.T) {
+	filePath, err := ioutil.TempDir(".", "vault")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	fmt.Printf("filePath: %q\n", filePath)
+	defer os.RemoveAll(filePath)
+
+	logger := logformat.NewVaultLogger(log.LevelTrace)
+
+	config := map[string]string{
+		"path": filePath,
+	}
+
+	storage, err := logical.NewLogicalStorage(logical.LogicalTypeFile, config, logger)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sp, err := NewStoragePackerV2(&Config{
+		View:             storage,
+		Logger:           log.New("storagepackertest"),
+		BucketCount:      256,
+		BucketShardCount: 32,
+		BucketMaxSize:    512 * 1024,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	count := 100
+	entity := &identity.Entity{
+		Metadata: map[string]string{
+			"samplekey1": "samplevalue1",
+			"samplekey2": "samplevalue2",
+			"samplekey3": "samplevalue3",
+			"samplekey4": "samplevalue4",
+			"samplekey5": "samplevalue5",
+		},
+	}
+
+	testPutItem(t, sp, count, entity)
+	testGetItem(t, sp, count, false)
+	testDeleteItem(t, sp, count)
+	testGetItem(t, sp, count, true)
 }
 
 func Benchmark_StoragePackerV2_Sharding(b *testing.B) {
@@ -123,119 +226,6 @@ func Benchmark_StoragePackerV2_Sharding(b *testing.B) {
 
 		if fetchedMessage.ID != id {
 			b.Fatalf("failed to fetch item ID: %q\n", id)
-		}
-	}
-}
-
-func TestStoragePackerV2_Delete(t *testing.T) {
-	filePath, err := ioutil.TempDir(".", "vault")
-	if err != nil {
-		t.Fatalf("err: %s", err)
-	}
-	fmt.Printf("filePath: %q\n", filePath)
-	//defer os.RemoveAll(filePath)
-
-	logger := logformat.NewVaultLogger(log.LevelTrace)
-
-	config := map[string]string{
-		"path": filePath,
-	}
-
-	storage, err := logical.NewLogicalStorage(logical.LogicalTypeFile, config, logger)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	sp, err := NewStoragePackerV2(&Config{
-		View:             storage,
-		Logger:           log.New("storagepackertest"),
-		BucketCount:      8,
-		BucketShardCount: 4,
-		BucketMaxSize:    512,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	count := 1000
-
-	for i := 1; i <= count; i++ {
-		if i%500 == 0 {
-			fmt.Printf("put item iteration: %d\n", i)
-		}
-		id := strconv.Itoa(i)
-		message := &identity.Entity{
-			ID: id,
-			Metadata: map[string]string{
-				"samplekey1": "samplevalue2",
-			},
-		}
-
-		marshaledMessage, err := ptypes.MarshalAny(message)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		item := &Item{
-			ID:      id,
-			Message: marshaledMessage,
-		}
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		_, err = sp.PutItem(item)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	for i := 1; i <= count; i++ {
-		if i%500 == 0 {
-			fmt.Printf("get item iteration: %d\n", i)
-		}
-		id := strconv.Itoa(i)
-		itemFetched, err := sp.GetItem(id)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if itemFetched == nil {
-			t.Fatalf("failed to read the inserted item; itemID: %q\n", id)
-		}
-
-		var fetchedMessage identity.Entity
-		err = ptypes.UnmarshalAny(itemFetched.Message, &fetchedMessage)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if fetchedMessage.ID != id {
-			t.Fatalf("failed to fetch item ID: %q\n", id)
-		}
-	}
-
-	for i := 1; i <= count; i++ {
-		if i%500 == 0 {
-			fmt.Printf("delete item iteration: %d\n", i)
-		}
-		id := strconv.Itoa(i)
-		err = sp.DeleteItem(id)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	for i := 1; i <= count; i++ {
-		if i%500 == 0 {
-			fmt.Printf("verify deletion; get item iteration: %d\n", i)
-		}
-		id := strconv.Itoa(i)
-		itemFetched, err := sp.GetItem(id)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if itemFetched != nil {
-			t.Fatalf("item should not exist; id: %q", id)
 		}
 	}
 }
